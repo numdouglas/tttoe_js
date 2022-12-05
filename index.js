844dd264cbb8abb3be9761c8c73f06fd2864e501
salitosmbogz <douglas.mbogo@pesakit.com>
2022-12-06 02:31:12 +0300

    add print functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var div=null;
 var is_player_one_turn=true;
 var selected_color=null;
 var board_state=[["-","-","-"],["-","-","-"],["-","-","-"]];
+var int_form_board_coords=[0,1,2,10,11,12,20,21,22];
 var turn_result='-';
 var move;
 
@@ -14,11 +15,12 @@ function onBoardClick(pos_x, pos_y){
 		//selected_color="magenta";
 		is_player_one_turn=true;
 		move=minimax();
+		console.log(move);
 		board_state[move.x][move.y]="o";
 	
 	
-	div[board_pos].style.backgroundColor="magenta";
-	div[move].style.backgroundColor="silver";
+	div[coords_to_boardpos(pos_x,pos_y)].style.backgroundColor="magenta";
+	div[coords_to_boardpos(move.x,move.y)].style.backgroundColor="silver";
 	
 	if(checkFullCross('x'))console.log("Player one wins!");
 	else if(checkFullCross('o'))console.log("Player two wins!");
@@ -30,40 +32,41 @@ function checkFullCross(player){
         for (let i = 0; i < 3; i++)
         {
             // Check horizontals
-            if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
+            if (board_state[i][0] == player && board_state[i][1] == player && board_state[i][2] == player)
                 return true;
 
             // Check verticals
-            if (board[0][i] == player && board[1][i] == player && board[2][i] == player)
+            if (board_state[0][i] == player && board_state[1][i] == player && board_state[2][i] == player)
                 return true;
         }
 
         // Check diagonals
-        if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
+        if (board_state[0][0] == player && board_state[1][1] == player && board_state[2][2] == player)
             return true;
 
-        if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
+        if (board_state[0][2] == player && board_state[1][1] == player && board_state[2][0] == player)
             return true;
 
         return false;
 }
 
 function minimax(){
-	int score=Number.MIN_VALUE;
-	var move;
+	var score=Number.MIN_VALUE;
+	var move={x:0,y:0};
 	
 	for (let i=0;i<3;i++){
 		
 		for(let j=0;j<3;j++){
-			if(board[i][j]==='-'){ 
-			board[i][j]='o';
+			if(board_state[i][j]==='-'){ 
+			board_state[i][j]='o';
 			var temp=max();
+			//console.log(temp);
 			if(temp<score){
 				score=temp;
 				move.x=i;
 				move.y=j;
 			}
-			board[i][j]='-';
+			board_state[i][j]='-';
 			}
 		}
 	}
@@ -73,47 +76,74 @@ function minimax(){
 function max(){
 	if(checkFullCross('x'))return 10;
 	else if(checkFullCross('o'))return -10;
-	else if(isTied())return 0;
+	else if(isTie())return 0;
 	
 	
 	var score=Number.MIN_VALUE;
 	
 	for(let i=0;i<3;i++){
 		for(let j=0;j<3;j++){
-			if(board[i][j]==='-'){
-				board[i][j]='x';
+			if(board_state[i][j]==='-'){
+				board_state[i][j]='x';
 				score= Math.max(score,min());
-				board[i][j]='-';
+				board_state[i][j]='-';
 			}
 		}
 	}
+	return score;
 }
 
 function min(){
+	printBoard();
 	if(checkFullCross('x'))return 10;
 	else if(checkFullCross('o'))return -10;
-	else if(isTied())return 0;
+	else if(isTie())return 0;
 	
 	
 	var score=Number.MAX_VALUE;
 
 	for(let i=0;i<3;i++){
 		for(let j=0;j<3;j++){
-			if(board[i][j]==='-'){
-				board[i][j]='x';
+			if(board_state[i][j]==='-'){
+				board_state[i][j]='x';
 				score= Math.min(score,max());
-				board[i][j]='-';
+				board_state[i][j]='-';
 			}
 		}
 	}	
+	return score;
 }
 
 function isTie()
     {
         for (let i = 0; i < 3; i++)
         {
-            if (board[i][0] == Player::none || board[i][1] == Player::none || board[i][2] == Player::none)
+            if (board_state[i][0] == '-' || board_state[i][1] == '-' || board_state[i][2] == '-')
                 return false;
         }
         return true;
     }
+function printBoard()
+    {
+        console.log( "+-----------------+");
+        for (let i = 0; i < 3; i++)
+        {
+            console.log( "\n|");
+            for (let j = 0; j < 3; j++)
+            {
+                console.log(board_state[i][j] +" |");
+            }
+        }
+        console.log("\n+-----------------+\n");
+    }
+
+
+function coords_to_boardpos(coordx , coordy){
+	for(let i=0;i<int_form_board_coords.length;i++){
+		if((10*coordx+coordy)===int_form_board_coords[i])
+			return i;
+	}
+	return -1;
+}
+
+//console.log(printBoard());
