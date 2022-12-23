b4c7243303d66650935e3ced8902c0025f9ff9ac
salitosmbogz <douglas.mbogo@pesakit.com>
2022-12-24 01:14:37 +0300

    ui enhancements

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const SERVER_DOMAIN=process.env.DOMAIN||"localhost";
 
 const io= require("socket.io")(server, {
 	cors: {
-	"origin":`http://${SERVER_DOMAIN}`
+	"origin":[`http://${SERVER_DOMAIN}`]
 	/*"methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
 	"credentials":"true",
 	"allowedHeaders":"Content-Type,Authorization"*/
