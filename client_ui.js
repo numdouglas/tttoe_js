5bed0dac7f7c94185ddbfb500f6066d122dc0550
salitosmbogz <douglas.mbogo@pesakit.com>
2022-12-28 14:10:01 +0300

    port switching

--- client_ui.js
+++ client_ui.js
@@ -1,4 +1,5 @@
-const DOMAIN=window.location.hostname;
+var DOMAIN=window.location.hostname;
+DOMAIN=DOMAIN!=="localhost"?DOMAIN:`${DOMAIN}:8080`
 //client dependencies
 const socket=io(`${DOMAIN}`);/*the port and http are used for purposes of local testing,
 										otherwise prod doesn't need them as traffic is proxied*/
